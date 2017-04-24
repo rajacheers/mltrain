5cdd537cc01002509fae9730d9351cc665a28c6f
AishwaryaSidhique <saismaat@gmail.com>
2017-04-24 23:39:09 +0530

    Arithmetic operators

---
+++ x.py
@@ -0,0 +1,159 @@
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
+
+
+
+2)For loop
+
+
+# Program to find the sum of all numbers
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+	sum = sum+val
+
+# Output: The sum is 48
+print("The sum is", sum)
+
+
+3)Range function
+
+
+# Output: range(0, 10)
+print(range(10))
+
+# Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(10)))
+
+# Output: [2, 3, 4, 5, 6, 7]
+print(list(range(2, 8)))
+
+# Output: [2, 5, 8, 11, 14, 17]
+print(list(range(2, 20, 3)))	
+
+
+
+4) While loop
+
+# Program to add natural
+n = 10
+
+# initialize sum and counter
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i+1    # update counter
+
+# print the sum(1+2+3+....+n)
+print("The sum is", sum)
+
+
+5) While with else
+
+# the use of else statement with the while loop
+
+counter = 0
+
+while counter < 3:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Outside loop")
+
+#output
+
+Inside loop
+Inside loop
+Inside loop
+Outside loop
+
+
+6) if-elif-else
+
+
+num = 3.4
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+#output 
+Positive number
+
+
+7) Nested if
+
+num = float(input("Enter a number: "))
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive number")
+else:
+    print("Negative number")
+
+
+8)Break statement
+
+
+# Use of break statement inside loop
+
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+
+
+#Output
+
+s
+t
+r
+
+
+9) Continue statement
+
+# Program to show the use of continue statement inside loops
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
+#Output
+
+s
+t
+r
+n
+g
+
+
