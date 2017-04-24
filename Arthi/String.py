d298cf8666f2033bf2e9fa5cecb53fb2fbe4fa50
Arthi2410 <arthisekarangsr@gmail.com>
2017-04-24 22:19:25 +0530

    python handson

---
+++ Arthi/String.py
@@ -0,0 +1,12 @@
+
+#program-1
+str1 = 'Artistic '
+str2 = 'Arthi'
+
+str1=str1+str2
+print(str1)
+
+#program-2
+filename = input("enter the file_name : ")
+ex = filename.split(".")
+print("extention of file is: ",ex[1])
